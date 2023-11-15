0339848b94ec032c435a8f801d2d5b6f8b888f3c
NoW0NDER <145441591+NoW0NDER@users.noreply.github.com>
2023-11-15 08:59:01 +0800

    update in treemap

--- src/components/graphs/treeMap.jsx
+++ src/components/graphs/treeMap.jsx
@@ -6,29 +6,60 @@ const TreeMap = ({data}) => {
 
     const children = data.children
 
-    const treemapOption = {
-      tooltip: {
-          trigger: 'item',
-          formatter: '{b}: {c}'
-      },
-      series: [
-        {
-          type: 'treemap',
-          id: 'rent_data_treemap',
-          animationDurationUpdate: 2000,
-          roam: false,
-          nodeClick: 'zoomToNode',
-          data: children,
-          universalTransition: true,
-          label: {
-            show: true
-          },
-          breadcrumb: {
-            show: false
+      const treemapOption = {
+        tooltip: {
+            trigger: 'item',
+            formatter: '{b}: {c}'
+        },
+        series: [
+          {
+            type: 'treemap',
+            id: 'rent_data_treemap',
+            animationDurationUpdate: 2000,
+            roam: true,
+            nodeClick: 'zoomToNode',
+            zoomToNodeRatio: 0.5, // Add zoomToNodeRatio property to enable zooming
+            data: children,
+            universalTransition: true,
+            label: {
+              show: true
+            },
+            breadcrumb: {
+              show: false
+            },
+            levels: [ // Add levels property to configure zoom levels
+              {
+                itemStyle: {
+                  borderWidth: 0,
+                  gapWidth: 5
+                },
+                upperLabel: {
+                  show: false
+                }
+              },
+              {
+                itemStyle: {
+                  borderWidth: 5,
+                  gapWidth: 1
+                },
+                emphasis: {
+                  itemStyle: {
+                    borderColor: '#fff'
+                  }
+                }
+              },
+              {
+                colorSaturation: [0.35, 0.5],
+                itemStyle: {
+                  borderWidth: 5,
+                  gapWidth: 1,
+                  borderColorSaturation: 0.6
+                }
+              }
+            ]
           }
-        }
-      ]
-  };
+        ]
+    };
 
     const sunburstOption = {
         series: [
@@ -57,7 +88,8 @@ const TreeMap = ({data}) => {
                 option={treemapOption}
                 style={{
                   height:'55vh',
-                  width:'100%'
+                  width:'100%',
+                  margin: '3vh'
                 }}
             />
         </div>
